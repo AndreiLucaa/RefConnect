6d92bf0fe49712c62fab93017b03fef9c3f6cb1d
AndreiLucaa <mihaiandreiluca16@yahoo.com>
2025-11-18 17:34:45 +0200

    Update Models: Add database entities and fix DateTime type

--- RefConnect/Models/GroupChat.cs
+++ RefConnect/Models/GroupChat.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 namespace RefConnect.Models;
 
 public class GroupChat
 {
+    [Key] public string idChat { get; set; }
+    public string numeChat { get; set; }
     
 }
--- RefConnect/Models/userReferee.cs
+++ RefConnect/Models/userReferee.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 namespace RefConnect.Models;
 
 public class userArbitru
 {
+    public enum Role
+    {
+        Admin,
+        Referee
+    }
     
+    [Key] public string userId { get; set; }
+    public Role role { get; set; }
+    public string nume { get; set; }
+    public string prenume { get; set; }
+    public string email { get; set; }
 }
